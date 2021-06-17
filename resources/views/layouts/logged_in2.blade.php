7001500746f7476a7c8d6dc5d168c959e8598c5b
Toyama4869 <85100206+Toyama4869@users.noreply.github.com>
2021-06-17 19:18:05 +0900

    Add files via upload

--- resources/views/layouts/logged_in2.blade.php
+++ resources/views/layouts/logged_in2.blade.php
@@ -2,47 +2,31 @@
 
 @section('header')
 <header>
-<div class="header_top">
-<img class="logo" src="{{ asset('images/logo.jpg') }}">
-<form class="search" method="post" action="{{ route('posts.find') }}">
+<nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+    <img class="logo" src="{{ asset('images/logo.png') }}">
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mainNav">
+      <ul class="navbar-nav">
+        {{-- <li class="nav-item"><li class="greeting">こんにちは、{{ \Auth::user()->name }}さん！</li></li> --}}
+        <li class="nav-item"><a href="{{ route('top', auth()->user()->id)}}" class="nav-link"><i class="fas fa-home"></i>ホーム</a></li>
+        <li class="nav-item"><a href="{{route('posts.create', auth()->user()->id)}}" class="nav-link"><i class="fas fa-plus-square"></i>新規出品</a></li>
+        <li class="nav-item"><a href="{{ route('users.show', auth()->user()->id)}}" class="nav-link"><i class="fas fa-user-circle"></i>プロフィール</a></li>
+        <li class="nav-item"><a href="{{ route('likes.index', auth()->user()->id) }}" class="nav-link"><i class="fas fa-bookmark"></i>お気に入り一覧</a></li>
+        <li class="nav-item"><a href="{{ route('users.exhibitions', auth()->user()->id) }}" class="nav-link"><i class="fas fa-list-alt"></i>出品商品一覧</a></li>
+        <li class="nav-item"><form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <input class="logout" type="submit" value="ログアウト">
+                    </form></li>
+      </ul>
+    <form class="search" method="post" action="{{ route('posts.find') }}">
     @csrf
-    <input type="text" name="input" value="{{$input}}">
+    <input type="text" name="input" value="">
     <input class="button" type="submit" value="商品検索">
     </form>
-    <ul class="header_nav">
-
-        <li>こんにちは、{{ \Auth::user()->name }}さん！</li>
-        <li>
-          <a href="{{ route('top', auth()->user()->id)}}">
-          ホーム
-          </a>
-        </li>
-         <li>
-        <a href="{{route('posts.create', auth()->user()->id)}}">
-            新規出品</a>
-        </li>
-        <li>
-          <a href="{{ route('users.show', auth()->user()->id)}}">
-          プロフィール
-          </a>
-        </li>
-        <li>
-          <a href="{{ route('likes.index', auth()->user()->id) }}">
-            お気に入り一覧
-          </a>
-        </li>
-        <li>
-          <a href="{{ route('users.exhibitions', auth()->user()->id) }}">
-            出品商品一覧
-          </a>
-        </li>
-        <li>
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <input class="logout" type="submit" value="ログアウト">
-            </form>
-        </li>
-    </ul>
-</div>
+</nav>
 </header>
+
+
 @endsection
--- resources/views/layouts/not_logged_in.blade.php
+++ resources/views/layouts/not_logged_in.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.default')
 
 @section('header')
-<header>
-    <ul class="header_nav">
-        <li>
-          <a href="{{ route('register') }}">
-            ユーザー登録
-          </a>
-        </li>
-        <li>
-          <a href="{{ route('login') }}">
-            ログイン
-          </a>
-        </li>
-    </ul>
-</header>
+
+<nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+    <img class="logo" src="{{ asset('images/logo.png') }}">
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mainNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link"><i class="fas fa-user"></i>ユーザー登録</a></li>
+        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link"><i class="fas fa-sign-in-alt"></i>ログイン</a></li>
+      </ul>
+</nav>
 @endsection
